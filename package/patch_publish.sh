d6e76eae2afd7d0ba63e39ee8390e1682ff61077
stread <stread.chou@gmail.com>
2024-11-21 11:24:19 +0800

    快速开发期: 初始化

---
+++ package/patch_publish.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890
+
+# 获取当前脚本所在的目录
+FILE_PATH=$(pwd)
+
+# 切换到 ./nest-curd 目录并执行 npm 命令
+cd "$FILE_PATH/nest-curd" || { echo "Failed to enter $FILE_PATH/nest-curd directory"; exit 1; }
+npm run patch && npm publish
+
+# 切换到 ./generator 目录并执行 npm 命令
+cd "$FILE_PATH/generator" || { echo "Failed to enter $FILE_PATH/generator directory"; exit 1; }
+npm run patch && npm publish
+
+# 切换到 ./quasar-curd 目录并执行 npm 命令
+cd "$FILE_PATH/quasar-curd" || { echo "Failed to enter $FILE_PATH/generator directory"; exit 1; }
+npm run patch && npm publish
+
+echo "Build and publish completed successfully."
