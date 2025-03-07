3f0e642de52eda5097c8bce8b0b634b2b64192ea
stread <stread.chou@gmail.com>
2025-03-07 14:49:11 +0800

    快速开发

--- example/nest/src/app.module.ts
+++ example/nest/src/app.module.ts
@@ -1,10 +1,28 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {ModuleList} from "./generator/collect/module.collect";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {EntityList} from "./generator/collect/entity.collect";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: 'localhost',
+            port: 3306,
+            username: 'root',
+            password: '123456',
+            database: 'test',
+            entities: [
+                ...EntityList,
+            ],
+            synchronize: true,
+        }),
+        ...ModuleList,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
